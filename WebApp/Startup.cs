d857bcd777219fe589ce95b90101599c27cc707b
Alexandra Luneva <Luneva.AV@mail.ru>
2021-04-16 10:45:27 +0700

    Chapter 30: Using Filters

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -6,6 +6,7 @@ using WebApp.Models;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Filters;
 
 namespace WebApp
 {
@@ -33,6 +34,12 @@ namespace WebApp
 
             services.Configure<MvcOptions>(opts => opts.ModelBindingMessageProvider
             .SetValueMustNotBeNullAccessor(value => "Please enter a value"));
+
+            services.AddScoped<GuidResponseAttribute>();
+            services.Configure<MvcOptions>(opts => {
+                opts.Filters.Add<HttpsOnlyAttribute>();
+                opts.Filters.Add(new MessageAttribute("This is the globally-scoped filter"));
+            });
         }
         public void Configure(IApplicationBuilder app, DataContext context, IAntiforgery antiforgery)
         {
